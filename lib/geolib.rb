b2762d42520e21163bace4aaea32fc1645d55453
Ben Griffiths <bengriffiths@gmail.com>
2011-03-16 11:56:16 +0000

    Commit library for Matt to see

--- lib/geolib.rb
+++ lib/geolib.rb
@@ -6,6 +6,8 @@ require 'geolib/google'
 require 'geolib/mapit'
 require 'geolib/utils'
 
+require 'json'
+
 module Geolib
 
   def self.map(provider,*args)
@@ -20,12 +22,65 @@ module Geolib
     Geolib::Geonames.new()
   end
 
-  def self.geo_stack
+  def self.gazetteer
     Geolib::Mapit.new()
   end
 
   def self.ip_locator
     Geolib::Hostip.new()
+  end
+
+  class FuzzyPoint
+    accuracies = [:point,:postcode,:ward,:council,:nation,:country]
+    attr_reader :lon, :lat, :accuracy
+
+    def initialize(lon,lat,accuracy)
+      raise ValueError unless accuracies.include?(accuracy)
+      @lon,@lat,@accuracy = lon, lat, accuracy
+    end
+
+    def to_json
+      {:lat=>lat,:lon=>lon,:accuracy=>accuracy}.to_json
+    end
+  end
+
+  class GeoStack
+
+    attr_accessor :point,:postcode,:ward,:council,:nation,:country,:wmc
+    attr_accessor :fuzzy_point
+
+    def initialize(fields)
+      # first time we create something, we might
+      # well only have the ip_address
+      if fields[:ip_address]
+        # do something
+      end
+    end
+
+    def self.from_hash(json)
+      # do something
+    end
+
+    def to_hash
+      {
+        :fuzzy_point => {:lat=>51,:lon=>0,:accuracy=>:postcode}
+        :country => "UK",
+        :nation  => "E",
+        :council => { :id => 2493, :name => "Greenwich Borough Council", :type => "LBO" },
+        :ward    => { :id => 8365, :name => "Greenwich West", :type => "LBW" },
+        :wmc     => { :id=> 65837, :name => "Greenwich and Woolwich", :type => "WMC" },
+        :postcode => "SE10 8UG",
+        :point => { :lat=>"51.476441375971447", :lon => "-0.01587542101038760" }
+      }
+    end
+
+    def new_info_received(fields)
+      if fields[:postcode]
+        # compare to current info and change if necessary
+      end
+      return self
+    end
+
   end
 
 end
